86b622ec81bfbabb5db69db20304a7df7b9a7fb7
shikaifeng <kaifeng15@163.com>
2014-07-14 14:48:20 +0800

    modify

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Mypaper::Application.routes.draw do
   resources :users
   root :to => 'users#index'
 
-  # mount Api::V1::Server => '/api/v1', :as => 'api_v1'
-  # get '/:controller(/:action(/:id))'
-  # post '/:controller(/:action(/:id))'
-  
+  mount Api::V1::Server => '/api/v1', :as => 'api_v1'
+
 end
--- lib/api/v1/user_controller.rb
+++ lib/api/v1/user_controller.rb
@@ -1,11 +1,21 @@
+# module Api::V1::UserController
+#     def self.registered(app)
+#         # /api/v1/hello
+#         app.get '/create' do
+#             "hello world!!!"
+#         end
+#     end
+# end
+
+
 module Api::V1::UserController
 
     def self.registered(app)
-         app.post '/users/create' do
-
-          @user = User.new(params[:user])
-          @user.save
-          redirect_to @user
+         app.get '/users/create' do
+        
+          @user = User.new(:name => "shikaifeng")
+            "hello world!!!!!#{@user.name}??????"
+          
         end
     end
 end
